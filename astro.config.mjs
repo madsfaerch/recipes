a8bb4d41b93141e892cbf504f221bc52dd5d839a
Bob <bob-the-assistant@proton.me>
2026-01-29 23:10:23 +0100

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://bob-the-assistant.github.io',
+  base: '/recipes',
   integrations: [tailwind()],
 });
